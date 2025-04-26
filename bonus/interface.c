2689e95875cd452b019a72a490e47d148bd63703
Mathias Dumoulin <mathias.dumoulin@epitech.eu>
2025-04-26 22:00:12 +0200

    feat (term interface): add the base for the input handling and display for a terminal

---
+++ bonus/interface.c
@@ -0,0 +1,186 @@
+#include <SFML/Graphics.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+#include <stdbool.h>
+
+#define MAX_LINES 30
+#define MAX_LINE_LENGTH 90
+#define MAX_LINE_SIZE (MAX_LINE_LENGTH * 8)
+
+typedef struct {
+    char lines[MAX_LINES][MAX_LINE_LENGTH];
+    int last_apartenance;
+    int apartenance[MAX_LINES];
+    int line_count;
+    int actual_ligne;
+} LineBuffer;
+
+static int get_size(char *line)
+{
+    int size = 0;
+
+    for (int i = 0; line[i]; i++) {
+        if (line[i] == '\t')
+            size += 8 * 4;
+        else
+            size += 8;
+    }
+    return size;
+}
+
+static void prompt(sfRenderWindow *window, sfFont *font, int i)
+{
+    sfText* text = sfText_create();
+    sfText_setString(text, "$> ");
+    sfText_setFont(text, font);
+    sfText_setCharacterSize(text, 13);
+    sfText_setPosition(text, (sfVector2f){10, 10 + i * 18});
+    sfText_setColor(text, sfWhite);
+    sfRenderWindow_drawText(window, text, NULL);
+    sfText_destroy(text);
+}
+
+int main()
+{    
+    // SFML setup
+    sfRenderWindow* window;
+    sfVideoMode mode = {800, 600, 32};
+    window = sfRenderWindow_create(mode, "Terminal Viewer", sfClose, NULL);
+    if (!window)
+        return 1;
+
+    sfFont* font = sfFont_createFromFile("/usr/share/fonts/liberation-mono/LiberationMono-Regular.ttf");
+    if (!font) {
+        printf("Cannot load font.\n");
+        sfRenderWindow_destroy(window);
+        return 1;
+    }
+
+    LineBuffer buffer = { .last_apartenance = -1, .line_count = 0, .actual_ligne = 0};
+    char current_line[MAX_LINE_LENGTH] = {0};
+    int current_pos = 0;
+    sfClock* cursor_clock = sfClock_create();
+
+    while (sfRenderWindow_isOpen(window)) {
+        sfEvent event;
+        while (sfRenderWindow_pollEvent(window, &event)) {
+            if (event.type == sfEvtClosed) {
+                sfRenderWindow_close(window);
+            } else if (event.type == sfEvtTextEntered) {
+                char c = event.text.unicode;
+                if (c == 13) { // \n
+                    current_line[current_pos] = '\0';
+                    if (buffer.line_count >= MAX_LINES) {
+                        memmove(buffer.lines, buffer.lines + 1, (MAX_LINES - 1) * MAX_LINE_LENGTH);
+                        buffer.line_count--;
+                    }
+                    buffer.apartenance[buffer.line_count] = buffer.last_apartenance + 1;
+                    strcpy(buffer.lines[buffer.line_count++], current_line);
+                    buffer.last_apartenance++;
+                    current_pos = 0;
+                    memset(current_line, 0, sizeof(current_line));
+                    buffer.actual_ligne = buffer.line_count;
+                } else if (c == 127 || c == 8) { // Backspace
+                    if (current_pos > 0) {
+                        current_pos--;
+                        current_line[current_pos] = '\0';
+                    } else if (buffer.actual_ligne < buffer.line_count) {
+                        buffer.line_count--;
+                        for (current_pos = 0; buffer.lines[buffer.line_count][current_pos]; current_pos++)
+                            current_line[current_pos] = buffer.lines[buffer.line_count][current_pos];
+                        memset(buffer.lines[buffer.line_count], 0, sizeof(char) * MAX_LINE_LENGTH);
+                    }
+                } else if (c == 9 || c >= 32 && c <= 126) { // Caractères imprimables + tab
+                    if (get_size(current_line) >= MAX_LINE_SIZE - 8 && buffer.line_count - buffer.actual_ligne >= MAX_LINES)
+                        continue;
+                    if (get_size(current_line) < MAX_LINE_SIZE - 8) {
+                        current_line[current_pos++] = c;
+                        current_line[current_pos] = '\0';
+                    } else {
+                        current_line[current_pos] = '\0';
+                        if (buffer.line_count >= MAX_LINES) {
+                            memmove(buffer.lines, buffer.lines + 1, (MAX_LINES - 1) * MAX_LINE_LENGTH);
+                            buffer.line_count--;
+                            buffer.actual_ligne--;
+                        }
+                        buffer.apartenance[buffer.line_count] = buffer.last_apartenance + 1;
+                        strcpy(buffer.lines[buffer.line_count++], current_line);
+                        current_pos = 0;
+                        memset(current_line, 0, sizeof(current_line));
+                    }
+                }
+            }
+        }
+
+        // Clear window
+        sfRenderWindow_clear(window, sfBlack);
+
+        // Affiche toutes les lignes précédentes
+        int apartenance = 0;
+        bool new = false;
+        for (int i = 0; i < buffer.actual_ligne; ++i) {
+            if (i == 0 || apartenance != buffer.apartenance[i]) {
+                prompt(window, font, i);
+                apartenance = buffer.apartenance[i];
+                new = true;
+            }
+            sfText* text = sfText_create();
+            sfText_setString(text, buffer.lines[i]);
+            sfText_setFont(text, font);
+            sfText_setCharacterSize(text, 13);
+            sfText_setPosition(text, (sfVector2f){10 + 8 * 3 * new, 10 + i * 18});
+            sfText_setColor(text, sfWhite);
+            sfRenderWindow_drawText(window, text, NULL);
+            sfText_destroy(text);
+            new = false;
+        }
+
+        // Affiche la ligne en cours d'écriture
+        prompt(window, font, buffer.actual_ligne);
+        for (int i = buffer.actual_ligne; i < buffer.line_count; ++i) {
+            sfText* text = sfText_create();
+            sfText_setString(text, buffer.lines[i]);
+            sfText_setFont(text, font);
+            sfText_setCharacterSize(text, 13);
+            sfText_setPosition(text, (sfVector2f){10 + 8 * 3 * (i == buffer.actual_ligne), 10 + i * 18});
+            sfText_setColor(text, sfGreen);
+            sfRenderWindow_drawText(window, text, NULL);
+            sfText_destroy(text);
+        }
+
+        // Afficher le buffer actuel
+        sfText* text = sfText_create();
+        sfText_setString(text, current_line);
+        sfText_setFont(text, font);
+        sfText_setCharacterSize(text, 13);
+        sfText_setPosition(text, (sfVector2f){10 + 8 * 3 * (buffer.actual_ligne == buffer.line_count), 10 + buffer.line_count * 18});
+        sfText_setColor(text, sfGreen);
+        sfRenderWindow_drawText(window, text, NULL);
+        sfText_destroy(text);
+        
+
+        // Curseur clignotant
+        sfTime time = sfClock_getElapsedTime(cursor_clock);
+        if ((time.microseconds / 500000) % 2 == 0) { // clignote toutes les 0.5s
+            sfRectangleShape* cursor = sfRectangleShape_create();
+            sfRectangleShape_setSize(cursor, (sfVector2f){8, 13});
+            sfRectangleShape_setFillColor(cursor, sfGreen);
+            sfRectangleShape_setPosition(cursor, (sfVector2f){2.5 + 8 * 3 * (buffer.actual_ligne == buffer.line_count) + 8 + get_size(current_line), 10 + buffer.line_count * 18});
+            sfRenderWindow_drawRectangleShape(window, cursor, NULL);
+            sfRectangleShape_destroy(cursor);
+        }
+
+        sfRenderWindow_display(window);
+    }
+
+    // Cleanup
+    sfFont_destroy(font);
+    sfRenderWindow_destroy(window);
+    sfClock_destroy(cursor_clock);
+    return 0;
+}
