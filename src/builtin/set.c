7db08fd198f35a4c8d1b9568f8a24b04fb42f231
THomas Bourdeau <thomas.bourdeau@epitech.eu>
2025-05-06 10:56:57 +0200

    fix : the command set and unset now work

--- src/builtin/set.c
+++ src/builtin/set.c
@@ -6,7 +6,7 @@
 */
 
 #include "minishell.h"
-#include "../../include/error.h"
+#include "error.h"
 #include "printf.h"
 #include "memory.h"
 
@@ -21,10 +21,10 @@ static int display_var(main_data_t *data)
     if (!keys)
         return err_prog(UNDEF_ERR, KO, ERR_INFO);
     for (int i = 0; keys[i]; i++) {
-        value = ht_search(data->env, keys[i]);
+        value = ht_search(data->var, keys[i]);
         if (!value)
             return err_prog(UNDEF_ERR, KO, ERR_INFO);
-        if (my_printf("%s\t%s\n", keys[i], value) == KO)
+        if (my_printf("%s=%s\n", keys[i], value) == KO)
             return err_prog(UNDEF_ERR, KO, ERR_INFO);
     }
     if (free_hash_keys(keys) == KO)
@@ -40,12 +40,12 @@ int builtin_set(main_data_t *data, array_t *input, int start)
         data->return_value = OK;
         return display_var(data);
     }
-    if (input->len - start == 2) {
+    if (input->len - start == 3) {
         if (ht_insert(data->var, my_strdup(input->data[start + 1]),
             my_strdup(input->data[start + 2]), &free_hash_data_str) == KO)
             return err_prog(UNDEF_ERR, KO, ERR_INFO);
     }
-    if (input->len - start != 1 && input->len - start != 2) {
+    if (input->len - start == 2) {
         if (ht_insert(data->var, my_strdup(input->data[start + 1]),
             my_strdup(""), &free_hash_data_str) == KO)
             return err_prog(UNDEF_ERR, KO, ERR_INFO);
